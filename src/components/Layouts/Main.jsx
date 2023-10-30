bf150af0b36e78f11e86c036ee1f3607c3f1c2bc
SalehinShohan <ps.shohan1@gmail.com>
2023-10-30 11:58:59 +0600

    add popup

--- src/components/Layouts/Main.jsx
+++ src/components/Layouts/Main.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import FooterPage from "../FooterPage/FooterPage";
 import GoToTop from "../GoToTop/GoToTop";
+import Popup from "../PopUp/PopUp";
 
 const Main = () => {
   return (
@@ -9,6 +10,7 @@ const Main = () => {
       <Navbar></Navbar>
       <div className="min-h-[calc(100vh-137px)]">
         <Outlet></Outlet>
+        <Popup></Popup>
       </div>
       <FooterPage></FooterPage>
       <GoToTop></GoToTop>
