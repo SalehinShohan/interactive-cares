90bc997245640f14b8c5e57a107bdb564e3b5173
SalehinShohan <ps.shohan1@gmail.com>
2023-10-31 00:53:28 +0600

    final update

--- src/components/Layouts/Main.jsx
+++ src/components/Layouts/Main.jsx
@@ -3,10 +3,13 @@ import Navbar from "../Navbar/Navbar";
 import FooterPage from "../FooterPage/FooterPage";
 import GoToTop from "../GoToTop/GoToTop";
 import Popup from "../PopUp/PopUp";
+import { motion, useScroll } from "framer-motion";
 
 const Main = () => {
+  const { scrollYProgress } = useScroll();
   return (
     <div>
+      <motion.div style={{ scaleX: scrollYProgress }} />
       <Navbar></Navbar>
       <div className="min-h-[calc(100vh-137px)]">
         <Outlet></Outlet>
